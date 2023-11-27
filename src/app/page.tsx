218a410c80485134283afd0b8ea81569e6d60a4c
anoopraju31 <anoopraju31@gmail.com>
2023-11-27 19:17:27 +0530

    Home Page - Hero title added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
 export default function Home() {
 	return (
 		<main>
-			<h1> Dropbox Clone </h1>
+			<div className=''>
+				<div className='p-10 flex flex-col bg-[#2b2929] dark:bg-slate-800 text-white space-y-5'>
+					<h1 className='text-5xl font-bold'>
+						Welcome to Dropbox.
+						<br />
+						<br />
+						Storing everything for you and your business needs. All in one
+						place.
+					</h1>
+
+					<p className='pb-20'>
+						Enhance your personal storage with Dropbox, offering a simple and
+						efficient way to upload, organize, and access files from anywhere.
+						Securely store important documents and media, and experience the
+						convenience of easy file management and sharing in one centralized
+						solution.
+					</p>
+
+					<Link
+						href='/dashboard'
+						className='flex cursor-pointer bg-blue-500 p-5 w-fit'>
+						<span> Try it for free! </span>
+						<ArrowRight className='ml-10' />
+					</Link>
+				</div>
+			</div>
 		</main>
 	)
 }
