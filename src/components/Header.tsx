95c42da3cfed8b46c81b18fcefff22ba8b046e61
anoopraju31 <anoopraju31@gmail.com>
2023-11-27 18:18:42 +0530

    Header Component - Login-logout added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -17,6 +18,16 @@ const Header = () => {
 				</div>
 				<h1 className='font-bold text-xl'> Dropbox </h1>
 			</Link>
+
+			<div className='px-5 flex space-x-2 items-center'>
+				{/* Theme Toggler */}
+
+				<UserButton afterSignOutUrl='/' />
+
+				<SignedOut>
+					<SignInButton afterSignInUrl='/dashboard' mode='modal' />
+				</SignedOut>
+			</div>
 		</header>
 	)
 }
