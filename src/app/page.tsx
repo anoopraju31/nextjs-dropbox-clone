e67c0ae5ae5eaaa4902ec7ae30d6d6395ac06fb1
anoopraju31 <anoopraju31@gmail.com>
2023-11-27 19:26:30 +0530

    Home Page - Hero Section refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ArrowRight } from 'lucide-react'
 export default function Home() {
 	return (
 		<main>
-			<div className='flex flex-col lg:flex-row items-center bg-[#2b2929] dark:bg-slate-800'>
+			<section className='flex flex-col lg:flex-row items-center bg-[#2b2929] dark:bg-slate-800'>
 				<div className='p-10 flex flex-col bg-[#2b2929] dark:bg-slate-800 text-white space-y-5'>
 					<h1 className='text-5xl font-bold'>
 						Welcome to Dropbox.
@@ -39,7 +39,7 @@ export default function Home() {
 						Your browser does not support
 					</video>
 				</div>
-			</div>
+			</section>
 		</main>
 	)
 }
