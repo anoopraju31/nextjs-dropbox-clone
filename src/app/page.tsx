2213a56dbdc52a0f82d4a0acbcc113953a17f72b
anoopraju31 <anoopraju31@gmail.com>
2023-11-27 19:20:45 +0530

    Home Page - Hero video added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,16 @@ export default function Home() {
 						<ArrowRight className='ml-10' />
 					</Link>
 				</div>
+
+				<div className='bg-[#1E1919] dark:bg-slate-800 h-full p-10'>
+					<video autoPlay loop muted className='rounded-lg'>
+						<source
+							src='https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4'
+							type='video/mp4'
+						/>
+						Your browser does not support
+					</video>
+				</div>
 			</div>
 		</main>
 	)
