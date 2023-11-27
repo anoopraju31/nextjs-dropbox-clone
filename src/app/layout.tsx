bd2fcbee37f6710c9941d194686be3bd02d81324
anoopraju31 <anoopraju31@gmail.com>
2023-11-27 18:13:56 +0530

    Header Component - logo added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import Header from '@/components/Header'
 
 type Props = {
 	children: React.ReactNode
@@ -20,7 +21,10 @@ export default function RootLayout(props: Props) {
 	return (
 		<ClerkProvider>
 			<html lang='en'>
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<Header />
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<header className='flex items-center justify-between'>
+			<Link href='/' className='flex items-center space-x-2'>
+				<div className='bg-[#0160FE] w-fit'>
+					<Image
+						src='https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png'
+						alt='logo'
+						className='invert'
+						height={50}
+						width={50}
+					/>
+				</div>
+				<h1 className='font-bold text-xl'> Dropbox </h1>
+			</Link>
+		</header>
+	)
 }
 
 export default Header
