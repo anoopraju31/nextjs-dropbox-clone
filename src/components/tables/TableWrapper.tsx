3a9c444f3ff8438fc33185788244d9ade48a116c
anoopraju31 <anoopraju31@gmail.com>
2023-11-28 23:40:01 +0530

    Custom Hook - useFiles - created

--- src/components/tables/TableWrapper.tsx
+++ src/components/tables/TableWrapper.tsx
@@ -1,11 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { useUser } from '@clerk/nextjs'
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { collection, doc, orderBy, query } from 'firebase/firestore'
-import { Skeleton } from '@/components/ui/skeleton'
-import { db } from '../../../firebase'
+import useFiles from '@/hooks/useFiles'
 import { Button } from '../ui/button'
 import { DataTable } from './Table'
 import { columns } from './columns'
@@ -17,34 +12,7 @@ type Props = {
 
 const TableWrapper = (props: Props) => {
 	const { skeletonFiles } = props
-	const { user } = useUser()
-	const [initialFiles, setInitialFiles] = useState<FileType[]>([])
-	const [sort, setSort] = useState<'asc' | 'desc'>('desc')
-	const [docs, loading, error] = useCollection(
-		user &&
-			query(
-				collection(db, 'users', user.id, 'files'),
-				orderBy('timestamp', sort),
-			),
-	)
-
-	useEffect(() => {
-		if (!docs) return //defensive programming
-
-		const files: FileType[] = docs.docs.map((doc) => ({
-			id: doc.id,
-			filename: doc.data().filename || doc.id,
-			fullName: doc.data().fullName,
-			timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
-			downloadUrl: doc.data().downloadUrl,
-			size: doc.data().size,
-			type: doc.data().type,
-		}))
-
-		setInitialFiles(files)
-	}, [docs])
-
-	const handleSort = () => setSort((prev) => (prev === 'asc' ? 'desc' : 'asc'))
+	const { initialFiles, sort, handleSort, docs } = useFiles()
 
 	// Skeleton UI
 	if (docs?.docs.length === undefined)
--- src/hooks/useFiles.tsx
+++ src/hooks/useFiles.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useUser } from '@clerk/nextjs'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { collection, orderBy, query } from 'firebase/firestore'
+import { db } from '../../firebase'
 
 const useFiles = () => {
-	return <div>useFiles</div>
+	const { user } = useUser()
+	const [initialFiles, setInitialFiles] = useState<FileType[]>([])
+	const [sort, setSort] = useState<'asc' | 'desc'>('desc')
+	const [docs] = useCollection(
+		user &&
+			query(
+				collection(db, 'users', user.id, 'files'),
+				orderBy('timestamp', sort),
+			),
+	)
+
+	useEffect(() => {
+		if (!docs) return //defensive programming
+
+		const files: FileType[] = docs.docs.map((doc) => ({
+			id: doc.id,
+			filename: doc.data().filename || doc.id,
+			fullName: doc.data().fullName,
+			timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+			downloadUrl: doc.data().downloadUrl,
+			size: doc.data().size,
+			type: doc.data().type,
+		}))
+
+		setInitialFiles(files)
+	}, [docs])
+
+	const handleSort = () => setSort((prev) => (prev === 'asc' ? 'desc' : 'asc'))
+
+	return {
+		initialFiles,
+		sort,
+		handleSort,
+		docs,
+	}
 }
 
 export default useFiles
